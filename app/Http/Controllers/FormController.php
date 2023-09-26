c290fb43987459639190336d13f578c0afb36555
Arfi23 <arfifaizilraushani@gmail.com>
2023-09-26 18:43:34 +0700

    Membuat form.blade.php

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+    public function index()
+    {
+        return view('form');
+    }
+
+    public function show(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|name',
+            'notelp' => 'required|numeric',
+            'email' => 'required|email',
+            'password' => 'required|min:4',
+            'pekerjaan' => 'required',
+            'institusi' => 'required',
+            'double' => 'required|numeric|between:2.5,99.99',
+            'image' => 'required|max:2048|mimes:jpg,jpeg,png'
+        ]);
+
+        $request->image->storeAs('public/images', $request->image->getClientOriginalName());
+
+        $results = [
+            'nama' => $request->nama,
+            'notelp' => $request->notelp,
+            'email' => $request->email,
+            'password' => $request->password,
+            'pekerjaan' => $request->pekerjaan,
+            'institusi' => $request->institusi,
+            'double' => $request->double,
+            'image' => $request->image->getClientOriginalName(),
+        ];
+
+        return redirect("/result")->with(['results' => $results, 'status' => 'Form Submitted']);
+    }
+
+    public function result()
+    {
+        $results = session()->get('results');
+        $status = session()->get('status');
+
+        return view('result', compact('results', 'status'));
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/form', [FormController::class, 'index']);
+Route::post('/form', [FormController::class, 'show']);
+Route::get('/result', [FormController::class, 'result']);
+
