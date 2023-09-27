2536e1023d289bad758c2e767ad4d6b2446fca04
Arfi23 <arfifaizilraushani@gmail.com>
2023-09-27 09:59:03 +0700

    Menambahkan array message dan session untuk menyimpan riwayat pengisian

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -12,6 +12,26 @@ class FormController extends Controller
 
     public function show(Request $request)
     {
+        // dd($request->all());
+
+        $messages = [
+            'nama.required' => 'Kolom nama wajib diisi.',
+            'notelp.required' => 'Kolom nomor telepon wajib diisi.',
+            'notelp.numeric' => 'Kolom nomor telepon harus berisi angka.',
+            'email.required' => 'Kolom email wajib diisi.',
+            'email.email' => 'Format email tidak valid.',
+            'password.required' => 'Kolom password wajib diisi.',
+            'password.min' => 'Password harus terdiri dari minimal :min karakter.',
+            'pekerjaan.required' => 'Kolom pekerjaan wajib diisi.',
+            'institusi.required' => 'Kolom institusi wajib diisi.',
+            'double.required' => 'Kolom bilangan wajib diisi.',
+            'double.numeric' => 'Kolom bilangan harus berupa angka dan menggunakan format koma berupa titik',
+            'double.between' => 'Kolom bilangan harus antara :min dan :max.',
+            'image.required' => 'Pilih gambar untuk diunggah.',
+            'image.max' => 'Ukuran gambar tidak boleh lebih dari :max kilobytes.',
+            'image.mimes' => 'Format gambar yang diizinkan hanya: jpg, jpeg, png.',
+        ];
+
         $request->validate([
             'nama' => 'required',
             'notelp' => 'required|numeric',
@@ -21,7 +41,9 @@ class FormController extends Controller
             'institusi' => 'required',
             'double' => 'required|numeric|between:2.50,99.99',
             'image' => 'required|max:2048|mimes:jpg,jpeg,png'
-        ]);
+        ], $messages);
+
+        // dd('Validation passed');
 
         $request->image->storeAs('public/images', $request->image->getClientOriginalName());
 
@@ -36,6 +58,8 @@ class FormController extends Controller
             'image' => $request->image->getClientOriginalName(),
         ];
 
+        session()->put('uploaded_file', $request->image->getClientOriginalName());
+
         return redirect("/result")->with(['results' => $results, 'status' => 'Form Submitted']);
     }
 
@@ -43,8 +67,9 @@ class FormController extends Controller
     {
         $results = session()->get('results');
         $status = session()->get('status');
+        $uploadedFileName = session()->get('uploaded_file');
 
-        return view('result', compact('results', 'status'));
+        return view('result', compact('results', 'status', 'uploadedFileName'));
     }
 }
 
